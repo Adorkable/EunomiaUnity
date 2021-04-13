61d0cce65a6e7f630a27a3df3b906372cafe9c6b
Ian G <yo.ian.g@gmail.com>
2021-04-13 10:40:41 -0400

    library auto-initializer

---
+++ Runtime/EunomiaUnityInitializer.cs
@@ -0,0 +1,11 @@
+﻿namespace EunomiaUnity
+{
+    public class EunomiaUnityInitializer : Initializer
+    {
+        private EunomiaUnity.Random random;
+        protected override void Perform()
+        {
+            random = new EunomiaUnity.Random(UnityEngine.Random.state, new Eunomia.WordList());
+        }
+    }
+}
