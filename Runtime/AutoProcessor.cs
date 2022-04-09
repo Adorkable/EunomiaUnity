eae2213e289581267ea02b874714a0fcb6c9fb91
Ian G <yo.ian.g@gmail.com>
2022-04-09 18:30:07 +0900

    AutoProcessor fixed for inheritance, support AfterProcessorFinished

--- Runtime/AutoProcessor.cs
+++ Runtime/AutoProcessor.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 // ReSharper disable once CheckNamespace
@@ -12,22 +13,30 @@ namespace EunomiaUnity
             Awake,
             Start,
             OnDestroy,
+            AfterProcessorFinished,
             None
         }
         [SerializeField]
         private AutoProcessTime autoProcessTime = AutoProcessTime.Start;
 
+        [SerializeField]
+        private Processor afterProcessorFinished;
+
         protected abstract void AutoProcess();
 
-        void Awake()
+        protected virtual void Awake()
         {
             if (autoProcessTime == AutoProcessTime.Awake)
             {
                 AutoProcess();
             }
+            else if (autoProcessTime == AutoProcessTime.AfterProcessorFinished)
+            {
+                StartCoroutine(AfterProcessorFinishedCoroutine());
+            }
         }
 
-        void Start()
+        protected virtual void Start()
         {
             if (autoProcessTime == AutoProcessTime.Start)
             {
@@ -35,12 +44,18 @@ namespace EunomiaUnity
             }
         }
 
-        void OnDestroy()
+        protected virtual void OnDestroy()
         {
             if (autoProcessTime == AutoProcessTime.OnDestroy)
             {
                 AutoProcess();
             }
+        }
+
+        IEnumerator AfterProcessorFinishedCoroutine()
+        {
+            yield return new WaitUntil(() => afterProcessorFinished.HasFinished);
+            AutoProcess();
         }
     }
 }
